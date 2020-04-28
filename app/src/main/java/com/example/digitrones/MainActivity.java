31e2c554e1f0a38effbbef2cda085d55e5c7658f
i-am-Sirius-Black <khanjaved.sk19@gmail.com>
2020-04-29 00:14:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/digitrones/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.digitrones;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings= webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://digitrones.com/");
+        webView.setWebViewClient(new WebViewClient());
+    }
+//    code for back pressed button
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+
+    }
+}
